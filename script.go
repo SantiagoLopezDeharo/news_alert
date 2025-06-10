3f6d65373a452744c0803e2c7218f4c280fbc4dc
santiago lopez de haro <santiagolhgpro@gmail.com>
2025-06-09 21:14:17 -0300

    feat(Medias): Added more medias for extractions

--- script.go
+++ script.go
@@ -1,12 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
 )
+
+func saveList(filename string, list []string) error {
+	data, err := json.MarshalIndent(list, "", "  ")
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filename, data, 0644)
+}
+
+func loadList(filename string) ([]string, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// Create empty file with [] content
+			emptyList := []string{}
+			emptyData, _ := json.Marshal(emptyList)
+			err = ioutil.WriteFile(filename, emptyData, 0644)
+			if err != nil {
+				return nil, err
+			}
+			return emptyList, nil
+		}
+		return nil, err
+	}
+
+	var list []string
+	err = json.Unmarshal(data, &list)
+	return list, err
+}
 
 func any_contains(s []string, cl []string) bool {
 	flag := false
@@ -34,16 +65,53 @@ func bbc() [][]string {
 		panic(err)
 	}
 
-	//fmt.Println(string(body))
-
-	re := regexp.MustCompile(`<a.*?href="(.*?)".*?>.*?<h2 data-testid="card-headline".*?>(.*?)</h2>`)
+	re := regexp.MustCompile(`<a.*?href="([^"]*)".*?>.*?<h2 data-testid="card-headline".*?>(.*?)</h2>`)
 
 	return re.FindAllStringSubmatch(string(body), -1)
 
 }
 
+func theguardian() [][]string {
+	url := "https://www.theguardian.com"
+	resp, err := http.Get(url + "/international")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	re := regexp.MustCompile(`<a href="([^"]*)".*?aria-label="(.*?)".*?></a>`)
+	return re.FindAllStringSubmatch(string(body), -1)
+}
+
+func nytimes() [][]string {
+	url := "https://www.nytimes.com"
+	resp, err := http.Get(url + "/international")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	re := regexp.MustCompile(`<div class="css-cfnhvx"><a.*?href="([^"]*)"><div.*?><p.*?>([A-Za-z0-9 ]+)</p></div></a></div>`)
+	return re.FindAllStringSubmatch(string(body), -1)
+}
+
 func main() {
-	cl := []string{"trump", "flowers", "nukes"}
+	cl, _ := loadList("list.json")
+
 	bbc_r := bbc()
 
 	for _, match := range bbc_r {
@@ -52,4 +120,23 @@ func main() {
 			fmt.Println()
 		}
 	}
+
+	tg := theguardian()
+
+	for _, match := range tg {
+		if len(match) > 2 && any_contains(match, cl) {
+			fmt.Println(match[2] + " -->  www.theguardian.com" + match[1])
+			fmt.Println()
+		}
+	}
+
+	nyt := nytimes()
+
+	for _, match := range nyt {
+		if len(match) > 2 {
+			fmt.Println(match[2] + " --> " + match[1])
+			fmt.Println()
+		}
+	}
+
 }
